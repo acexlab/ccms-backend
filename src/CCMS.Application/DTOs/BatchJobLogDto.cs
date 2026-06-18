7ce0beeac0cd0f1057e56160334f184be6572d84
Ershad s <ershadpersonal123@gmail.com>
2026-06-18 14:17:35 +0530

    added a change

--- src/CCMS.Application/DTOs/BatchJobLogDto.cs
+++
@@ -1,5 +0,0 @@
-/*
- * File: BatchJobLogDto.cs
- * Description: Data transfer object representing the execution audit log of a background batch job.
- * To Implement: Keep in sync with BatchJobLog entity.
- */
